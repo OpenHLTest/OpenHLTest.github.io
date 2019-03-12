d85dd60d92dee2f31aa1d4f5ecdacc1a7b4ca078
Travis CI User <travis@example.org>
2019-03-12 21:28:45 +0000

    "update python client documentation"

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! e:\oht\doc-browser\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/travis/build/OpenHLTest/data-models/doc-browser/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
